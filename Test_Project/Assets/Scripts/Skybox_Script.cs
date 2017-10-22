02f7b2d85d43b3407d18508361d6a199e8090eb6
Unknown <andrew.rodiek@gmail.com>
2017-10-22 14:25:36 -0400

    Hand models and stuff

---
+++ Test_Project/Assets/Scripts/Skybox_Script.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Skybox_Script : MonoBehaviour
+{
+
+    SerialPort mySPort = new SerialPort("COM4", 9600);
+
+    public Material skyDefault;
+    public Material skyOne;
+
+    // Use this for initialization
+    void Start()
+    {
+        mySPort.Open();
+        RenderSettings.skybox = skyDefault;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string dataFromArduinoString = mySPort.ReadLine();
+        controlObjects(dataFromArduinoString);
+    }
+
+    void controlObjects(string switchData)
+    {
+        switch (switchData)
+        {
+            case "OnOff1val - ON":
+                RenderSettings.skybox = skyOne;
+                break;
+            default:
+                RenderSettings.skybox = skyDefault;
+                break;
+        }
+    }
+}
--- Test_Project/Assets/Uniduino/Scripts/Arduino.Firmata.cs
+++ Test_Project/Assets/Uniduino/Scripts/Arduino.Firmata.cs
@@ -1,8 +1,8 @@
 /*
  * Arduino.cs - Arduino/firmata library for Visual C# .NET
- * Copyright (C) 2009 Tim Farley 
+ * Copyright (C) 2009 Tim Farley
  * Copyright (C) 2013 Daniel MacDonald (major overhaul for Uniduino)
- * 
+ *
  * Special thanks to David A. Mellis, on whose Processing library
  * this code was is based.
  *
@@ -21,8 +21,8 @@
  * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
  * Boston, MA  02111-1307  USA
  *
- * 
- * 
+ *
+ *
  * $Id$
  */
 
@@ -39,29 +39,29 @@ using System.IO.Ports;
 
 namespace Uniduino
 {
-	public enum PinMode 
-	{ 	
-		INPUT = 0, 
-		OUTPUT = 1, 
+	public enum PinMode
+	{
+		INPUT = 0,
+		OUTPUT = 1,
 		ANALOG = 2,
 		PWM = 3,
 		SERVO = 4,
 		SHIFT = 5, // not implemented yet
 		I2C = 6,
-		
+
 	};
-	
+
 	/// <summary>
-	/// The scripting interface to an Arduino board connected via a serial port and running Firmata firmware. 
+	/// The scripting interface to an Arduino board connected via a serial port and running Firmata firmware.
 	/// </summary>
     public partial class Arduino : ArduinoBase
     {
 
 		public Action<string> Log = (s) => {};          // redirect logging output
-						
+
 		public static int LOW              = 0;
         public static int HIGH             = 1;
-		
+
 		private const int BATCH_PROCESS_BYTES_LIMIT = 64; // process no more than this many bytes per individual processInput call
         private const int MAX_DATA_BYTES         = 4096; // make this larger if receiving very large sysex messages
         private const int DIGITAL_MESSAGE       = 0x90; // send data for a digital port
@@ -73,10 +73,10 @@ namespace Uniduino
         private const int SYSTEM_RESET          = 0xFF; // reset from MIDI
         private const int START_SYSEX           = 0xF0; // start a MIDI SysEx message
         private const int END_SYSEX             = 0xF7; // end a MIDI SysEx message
-		
-		private const int CAPABILITY_QUERY      = 0x6B; // capabilities query for all pins		
-		private const int CAPABILITY_RESPONSE   = 0x6C; // 		
-		        
+
+		private const int CAPABILITY_QUERY      = 0x6B; // capabilities query for all pins
+		private const int CAPABILITY_RESPONSE   = 0x6C; //
+
         private SerialPort _serialPort;
         private int delay;
 
@@ -86,7 +86,7 @@ namespace Uniduino
         private int[] storedInputData = new int[MAX_DATA_BYTES];
         private bool parsingSysex;
         private int sysexBytesRead;
-		
+
 		// TODO: make these defaults + constructor parameters if people want LOTS of pins
         private /*volatile*/ int[] digitalOutputData = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         private /*volatile*/ int[] digitalInputData  = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -95,12 +95,12 @@ namespace Uniduino
 		// firmata protocol version
         private int majorVersion = 0;
         private int minorVersion = 0;
-		
-		public int MajorVersion { get { return majorVersion; } } 
-		public int MinorVersion { get { return minorVersion; } } 
-		
+
+		public int MajorVersion { get { return majorVersion; } }
+		public int MinorVersion { get { return minorVersion; } }
+
 		private bool can_poll_bytes_to_read = true; // store results of platform comparison...not available under all platforms (windows)
-			
+
 		/// <summary>
 		/// Default constructor, convenient for when embedded in a MonoBehavior with different construction behavior
 		/// </summary>
@@ -119,30 +119,30 @@ namespace Uniduino
 
 		public Arduino(string serialPortName, Int32 baudRate, bool autoStart, int delay)
 		{
-			connect(serialPortName, baudRate, autoStart, delay);	
+			connect(serialPortName, baudRate, autoStart, delay);
 		}
-			
+
         protected void connect(string serialPortName, Int32 baudRate, bool autoStart, int delay)
         {
             _serialPort = new SerialPort(serialPortName, baudRate);
 			//_serialPort = Win32SerialPort.CreateInstance();
-			
+
 			_serialPort.DtrEnable = true; // win32 hack to try to get DataReceived event to fire
-			_serialPort.RtsEnable = true; 
+			_serialPort.RtsEnable = true;
 			_serialPort.PortName = serialPortName;
 			_serialPort.BaudRate = baudRate;
-			
+
             _serialPort.DataBits = 8;
             _serialPort.Parity = Parity.None;
             _serialPort.StopBits = StopBits.One;
 			_serialPort.ReadTimeout = 1; // since on windows we *cannot* have a separate read thread
 			_serialPort.WriteTimeout = 1000;
-			
-			
+
+
 			// HAX: cant use compile time flags here, so cache result in a variable
 			if (UnityEngine.Application.platform.ToString().StartsWith("Windows"))
 				can_poll_bytes_to_read = false;
-			
+
             if (autoStart)
             {
                 this.delay = delay;
@@ -174,12 +174,12 @@ namespace Uniduino
         protected void Open()
         {
             _serialPort.Open();
-			
+
 			if (_serialPort.IsOpen)
 			{
 	            Thread.Sleep(delay);
-				
-			
+
+
 			}
         }
 
@@ -188,17 +188,17 @@ namespace Uniduino
         /// </summary>
         protected void Close()
         {
-			
+
 			if (_serialPort != null)
             	_serialPort.Close();
         }
-		
+
 		/// <summary>
 		/// True if this instance has an open serial port; does not imply that what we are actually connected
 		/// to a properly configured Arduino running firmata.
 		/// </summary>
 		public bool IsOpen { get { return _serialPort != null && _serialPort.IsOpen; } }
-		
+
         /// <summary>
         /// Lists all available serial ports on current system by calling SerialPort.GetPortNames(). This may not reliable across all platforms and usb serial capable devices.
         /// </summary>
@@ -233,18 +233,18 @@ namespace Uniduino
         /// </summary>
         /// <param name="pin">The arduino pin.</param>
         /// <param name="mode">Mode Arduino.INPUT or Arduino.OUTPUT.</param>
-        ///         
-        public void pinMode(int pin, PinMode mode) 
+        ///
+        public void pinMode(int pin, PinMode mode)
 		{ 	pinMode(pin, (int)mode); 	}
-		
+
         public void pinMode(int pin, int mode)
         {
-			/*	 
+			/*
 			 * 1  set digital pin mode (0xF4) (MIDI Undefined)
 			 * 2  pin number (0-127)
 			 * 3  state (INPUT/OUTPUT/ANALOG/PWM/SERVO, 0/1/2/3/4)
 		     */
-			
+
             byte[] message = new byte[3];
             message[0] = (byte)(SET_PIN_MODE);
             message[1] = (byte)(pin);
@@ -252,16 +252,16 @@ namespace Uniduino
             _serialPort.Write(message, 0, 3);
             message = null;
         }
-		
-		
-		
-		
+
+
+
+
 		/// <summary>
 		/// Enable/disable rfe of each port. When a port is enabled, the default Firmata firmware will continuously scan each digital input on the port and send updated values if any change.
 		/// Refer to arduino docs for discussion on ports and their mapping.
 		/// </summary>
 		/// <param name='port'>
-		/// Port number. 
+		/// Port number.
 		/// </param>
 		/// <param name='enable'>
 		/// Enable (0 or 1)
@@ -271,38 +271,38 @@ namespace Uniduino
 			byte[] command = new byte[2];
             command[0] = (byte)(REPORT_DIGITAL | port);
             command[1] = (byte)enable;
-            _serialPort.Write(command, 0, 2);        
+            _serialPort.Write(command, 0, 2);
 		}
-		
+
 		/// <summary>
 		/// Reports the analog.
 		/// </summary>
 		/// <param name='pin'>
-		/// Pin number in analog numbering scheme. 
+		/// Pin number in analog numbering scheme.
 		/// </param>
 		/// <param name='enable'>
 		/// Enable (0 or 1)
-		/// </param>		
+		/// </param>
 		public void reportAnalog(int pin, byte enable)
 		{
 			byte[] command = new byte[2];
             command[0] = (byte)(REPORT_ANALOG | pin);
             command[1] = (byte)enable;
-            _serialPort.Write(command, 0, 2);        
+            _serialPort.Write(command, 0, 2);
 		}
-		
+
 		/// <summary>
-		/// Request the device to report the version of the loaded Firmata firmware. 
+		/// Request the device to report the version of the loaded Firmata firmware.
 		/// The VersionDataReceived event is fired when version data is received.
 		/// </summary>
 		public void reportVersion()
-		{		
+		{
 			byte[] command = new byte[1];
 			command[0] = (byte)REPORT_VERSION;
-            _serialPort.Write(command, 0, 1);            
+            _serialPort.Write(command, 0, 1);
 		}
-		
-		
+
+
         /// <summary>
         /// Write to a digital pin that has been toggled to output mode with pinMode() method.
         /// </summary>
@@ -322,7 +322,7 @@ namespace Uniduino
             message[1] = (byte)(digitalOutputData[portNumber] & 0x7F);
             message[2] = (byte)(digitalOutputData[portNumber] >> 7);
             _serialPort.Write(message, 0, 3);
-			
+
         }
 
         /// <summary>
@@ -338,8 +338,8 @@ namespace Uniduino
             message[2] = (byte)(value >> 7);
             _serialPort.Write(message, 0, 3);
         }
-				
-		
+
+
 
 		/// <summary>
 		/// Digital data received event handler. Fired when new digital data is received from the device.
@@ -349,36 +349,36 @@ namespace Uniduino
         /// <param name="portData">A bit vector encoding the current value of all input pins on the port.</param>
 
 		public delegate void DigitalDataReceivedEventHandler(int portNumber, int portData);
-		public event DigitalDataReceivedEventHandler DigitalDataReceived;		
-		
+		public event DigitalDataReceivedEventHandler DigitalDataReceived;
+
         private void setDigitalInputs(int portNumber, int portData)
         {
 			//Log("received digital data");
             digitalInputData[portNumber] = portData;
-			if (DigitalDataReceived != null) DigitalDataReceived(portNumber, portData);	
+			if (DigitalDataReceived != null) DigitalDataReceived(portNumber, portData);
         }
 
 		/// <summary>
 		/// Analog data received event handler. Fired when new analog data is received from the device
 		/// This event will only be fired for analog pins for which reporting has been enabled with reportAnalog
-		/// </summary>		
+		/// </summary>
 		public delegate void AnalogDataReceivedEventHandler(int pin, int value);
 		public event AnalogDataReceivedEventHandler AnalogDataReceived;
-		
+
         private void setAnalogInput(int pin, int value)
         {
             analogInputData[pin] = value;
 			if (AnalogDataReceived != null) AnalogDataReceived(pin, value);
         }
-		
+
 		/// <summary>
 		/// Version data received event handler. Fired when version data is received.
 		/// Request protocol version data with a call to reportVersion
 		/// </summary>
-		
+
 		public delegate void VersionDataReceivedEventHandler(int majorVersion, int minorVersion);
 		public event VersionDataReceivedEventHandler VersionDataReceived;
-		
+
         private void setVersion(int majorVersion, int minorVersion)
         {
             this.majorVersion = majorVersion;
@@ -386,44 +386,44 @@ namespace Uniduino
 			if (VersionDataReceived != null) VersionDataReceived(majorVersion, minorVersion);
         }
 
-				
+
 		/// <summary>
-		/// Poll and process present input directly. Must be called repeatedly 
+		/// Poll and process present input directly. Must be called repeatedly
 		/// from same thread as created the object. (From the SerialPort documentation)
-		/// Experimentally, polling from a separate thread works on OSX but *will die 
+		/// Experimentally, polling from a separate thread works on OSX but *will die
 		/// horribly* on windows. Also, BytesToRead does not currently function under
 		/// windows so each call will cost 1ms (ReadTimeout) if no data is present.
-		// 
+		//
 		/// </summary>
 		void processInput()
-		{	
+		{
 			// dont let this loop block the entire thread if the input is coming in faster than we can read it
 			int processed = 0;
-			while(processed < BATCH_PROCESS_BYTES_LIMIT) 
+			while(processed < BATCH_PROCESS_BYTES_LIMIT)
 			{
 				processed++;
-				
+
 				if (can_poll_bytes_to_read && _serialPort.BytesToRead == 0)
-					return; 
-						
+					return;
+
 	           	//if (_serialPort.BytesToRead > 0) // windows fail
-	            {	
-	               
+	            {
+
 	                int inputData;
 					int command;
-					try 
-					{						
+					try
+					{
 						inputData = _serialPort.ReadByte();
 					} catch (Exception e) // swallow read exceptions
 					{
-						//Log(e.GetType().Name + ": "+e.Message);	
+						//Log(e.GetType().Name + ": "+e.Message);
 						if (e.GetType() == typeof(TimeoutException))
 							return;
-						else 
-							throw;							
-										
+						else
+							throw;
+
 					}
-						
+
 					//Log("readbyte: " + inputData.ToString());
 	                if (parsingSysex)
 	                {
@@ -443,7 +443,7 @@ namespace Uniduino
 	                {
 	                    waitForData--;
 	                    storedInputData[waitForData] = inputData;
-	                    
+
 	                    if (executeMultiByteCommand != 0 && waitForData == 0)
 	                    {
 	                        //we got everything
@@ -474,32 +474,32 @@ namespace Uniduino
 	                        command = inputData;
 	                        // commands in the 0xF* range don't use channel data
 	                    }
-						
+
 						//if (command == DIGITAL_MESSAGE) Log("Received digital message");
-	                    switch (command)								
+	                    switch (command)
 	                    {
 						case START_SYSEX:
 							Log("received START_SYSEX");
 							parsingSysex = true;
 							sysexBytesRead = 0;
 							break;
-	                    case DIGITAL_MESSAGE:									
+	                    case DIGITAL_MESSAGE:
 	                    case ANALOG_MESSAGE:
-	                    case REPORT_VERSION:								
+	                    case REPORT_VERSION:
 	                        waitForData = 2;
 	                        executeMultiByteCommand = command;
 	                        break;
 	                    }
 	                }
-	            } 
+	            }
 			}
         }
-		
+
 		void processSysexMessage()
 		{
 			Log("processing sysex 0x" + storedInputData[0].ToString("X2"));
-			// storedInputData will contain just the content of the message, minus the START/END_SYSEX bytes				
-			
+			// storedInputData will contain just the content of the message, minus the START/END_SYSEX bytes
+
 			switch(storedInputData[0])
 			{
 				/* capabilities response
@@ -515,7 +515,7 @@ namespace Uniduino
 				       127, until all pins implemented.
 				 * N  END_SYSEX (0xF7)
 				 */
-			case CAPABILITY_RESPONSE:							
+			case CAPABILITY_RESPONSE:
 				parseCapabilityResponse();
 				reallocatePinArrays(Pins);
 				if (CapabilitiesReceived != null) CapabilitiesReceived(Pins);
@@ -524,31 +524,34 @@ namespace Uniduino
 				break;
 			}
 		}
-		
-		
+		/**********************************************************/
+		public void UpdateSerial(){
+			processInput();
+		}
+		/**********************************************************/
 		void reallocatePinArrays(List<Pin> pins)
 		{
 			Array.Resize(ref digitalInputData, pins.Count);
 			Array.Resize(ref digitalOutputData, pins.Count);
 			Array.Resize(ref analogInputData, pins.Count); // slight hack: doesnt actually need to be this large since seldom are all digital pins also analog pins
-			
+
 		}
-		
-		
+
+
 		#region capabilities
-		
+
 		public List<Pin> Pins;	// list of Pins, populated by a successful capability query
-		
+
 		/// <summary>
 		/// Capabilities received event handler. Fired when pin capabilities are received from the device.
 		/// Request pin capabilities with a call to queryCapabilities
-		/// 
+		///
 		/// </summary>
 		public delegate void CapabilitiesReceivedEventHandler(List<Pin> pins);
 		public event CapabilitiesReceivedEventHandler CapabilitiesReceived;
-		
+
 		/// <summary>
-		/// Request firmware to report pin capabilities. 
+		/// Request firmware to report pin capabilities.
 		/// </summary>
 		public void queryCapabilities()
 		{
@@ -558,83 +561,82 @@ namespace Uniduino
             message[2] = (byte)(END_SYSEX);
             _serialPort.Write(message, 0, 3);
             message = null;
-        }		
-						
-		
+        }
+
+
 		/// <summary>
 		/// Pin description and capabilities
 		/// </summary>
 		public class Pin
 		{
-			/// pin number 
-			public int number;								
+			/// pin number
+			public int number;
 			/// pin number in analog input channel numbering; nonnegative only if this pin has analog capabilities
-			public int analog_number = -1;  				
+			public int analog_number = -1;
 			/// port number of this pin
-			public int port { get { return number/8; } }	
-			
+			public int port { get { return number/8; } }
+
 			/// <summary>
 			/// Firmata pin capability data
-			/// </summary> 
+			/// </summary>
 			public class Capability
 			{
-				public int mode; 
+				public int mode;
 				public PinMode Mode { get { return (PinMode)mode; } }
 				/// Pin input or output resolution in bits for this mode
-				public int resolution;	
+				public int resolution;
 			}
-			
+
 			// TODO: could make a field for state
-			
+
 			public List<Capability> capabilities = new List<Capability>();	/// List of supported capabilities for this pin
 		}
-		
-		
-		
+
+
+
 		void parseCapabilityResponse()
 		{
 			int k = 0;
 			int analog_channel_index = 0;
 			int offset = 0;
-		
+
 			Pins = new List<Pin>();
-			
+
 			offset++; // skip the command byte
 			while (offset < sysexBytesRead)
 			{
 				// parse a pin
 				var p = new Pin() { number = k };
 				Log("Adding pin " + k.ToString());
-				while(storedInputData[offset] != 127) 
-				{											
-					var pc = new Pin.Capability() 
-					{						
+				while(storedInputData[offset] != 127)
+				{
+					var pc = new Pin.Capability()
+					{
 						mode = storedInputData[offset],
 						resolution = storedInputData[offset+1]
 					};
 					p.capabilities.Add(pc);
-				
+
 					// number the analog pins as they are detected
 					if (pc.Mode == PinMode.ANALOG)
 					{
 						p.analog_number = analog_channel_index;
 						analog_channel_index++;
 					}
-						
+
 					Log("Added pin " + k.ToString() + " mode " + pc.Mode.ToString() + " with resolution " + pc.resolution.ToString() + "");
 					offset+=2;
-										
+
 				}
 				Pins.Add(p);
 				k++;
-				offset+=1; // skip 127 pin boundary byte							
-			}						
-						
+				offset+=1; // skip 127 pin boundary byte
+			}
+
 		}
-		
-		#endregion	
-	
+
+		#endregion
+
     } // End Arduino class
 
 } // End namespace
-
